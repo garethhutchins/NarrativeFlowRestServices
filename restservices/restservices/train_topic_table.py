9426a79703d480dade9dd492de154066747c5b78
Gareth Hutchins <Gareth.Hutchins@gmail.com>
2021-10-21 22:19:47 +0100

    Added Stem lem NMF model generation images

--- restservices/restservices/train_topic_table.py
+++ restservices/restservices/train_topic_table.py
@@ -29,17 +29,118 @@ from rest_framework import status
 
 #Get the text from the table file
 def get_table_text(table_file,column):
-        try:
-            df = pd.read_csv(table_file)
-            response = df
-        except Exception as e:
-            response = {"Message" : "Unable to Read CSV File {}".format(e)}
+    #First try to see if we can read the file in the correct format
+    #First try to read a csv file
+    try:
+        df = pd.read_csv(table_file)
+    except Exception as e:
+        response = {"Message" : "Unable to Read CSV File {}".format(e)}
+        status_code = status.HTTP_400_BAD_REQUEST
+        return response, status_code
+    #Now look for the column, which could be a name or numeric
+    #Check for empty string
+    if column == '':
+        if len(df.columns) == 0:
+            response = {"Message" : "Unable no columns in csv file"}
             status_code = status.HTTP_400_BAD_REQUEST
             return response, status_code
-        status_code = status.HTTP_200_OK
-        return response, status_code
+        if len(df.columns) > 1:
+            response = {"Message" : "Specify a Single Column which contains the text"}
+            status_code = status.HTTP_400_BAD_REQUEST
+            return response, status_code
+    #Now check to see if a column number was passed
+    if str.isdigit(column):
+        try:
+            i_column = int(column)
+            df = df[df.columns[i_column]]
+        except Exception as e:
+            response = {"Message" : "Unable to find column at position {}".format(str(i_column))}
+            status_code = status.HTTP_400_BAD_REQUEST
+            return response, status_code
+    else:
+        try:
+            df = df[column]
+        except Exception as e:
+            response = {"Message" : "Unable to find column at position {}".format(column)}
+            status_code = status.HTTP_400_BAD_REQUEST
+            return response, status_code
+    response = df
+    status_code = status.HTTP_200_OK
+    return response, status_code
 
+#A function to perform porter stemming
+def remove_stop_stem(text):
+    porter = PorterStemmer()
+    stop_words = set(stopwords.words('english'))
+    words = word_tokenize(text)
+    keepWords = []
+    for r in words:
+        if not r in stop_words:
+            if r.isalpha():
+                stemmed = porter.stem(r.lower())
+                keepWords.append(stemmed)
+    #convert it into a flat string
+    kwstring = ' '.join(keepWords)
+    return kwstring
 
+#Now create a function to lemmatise and tokenize the text
+def remove_stop_lem(text):
+    stop_words = set(stopwords.words('english'))
+    #words = text.split()
+    words = word_tokenize(text)
+    keepWords = []
+    for r in words:
+        if not r in stop_words:
+            if r.isalpha():
+                lemmatized = lemmatizer.lemmatize(r.lower())
+                keepWords.append(lemmatized)
+    #convert it into a flat string
+    kwstring = ' '.join(keepWords)
+    return kwstring 
+
+#This example came from the SKlearn tutorial
+#https://scikit-learn.org/stable/auto_examples/applications/plot_topics_extraction_with_nmf_lda.html
+
+def plot_top_words(model, feature_names, n_top_words, title,num_topics,topic_names):
+    rows = math.ceil(num_topics/5)
+    fs = 15*rows
+    fig, axes = plt.subplots(rows, 5, figsize=(fs, fs), sharex=True)
+    axes = axes.flatten()
+    for topic_idx, topic in enumerate(model.components_):
+        top_features_ind = topic.argsort()[:-n_top_words - 1:-1]
+        top_features = [feature_names[i] for i in top_features_ind]
+        weights = topic[top_features_ind]
+
+        ax = axes[topic_idx]
+        ax.barh(top_features, weights, height=0.7)
+        if len(topic_names) == 0:
+            ax.set_title(f'Topic {topic_idx +1}',
+                     fontdict={'fontsize': 30})
+        else:
+            topic_title = topic_names[topic_idx +1]
+            ax.set_title(topic_title,
+                     fontdict={'fontsize': 30})
+        ax.invert_yaxis()
+        ax.tick_params(axis='both', which='major', labelsize=20)
+        for i in 'top right left'.split():
+            ax.spines[i].set_visible(False)
+        fig.suptitle(title, fontsize=40)
+
+    plt.subplots_adjust(top=0.90, bottom=0.05, wspace=0.90, hspace=0.3)
+    return plt    
+
+#Train the NMF model
+def train_nmf(df,num_topics):
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf = tfidf_vectorizer.fit_transform(df)
+    tfidf_feature_names=tfidf_vectorizer.get_feature_names()
+    nmf = NMF(n_components=num_topics, random_state=1,
+          alpha=.1, l1_ratio=.5).fit(tfidf)
+    n_top_words = 10
+    plot_image = plot_top_words(nmf, tfidf_feature_names, n_top_words,
+               'Topics in NMF model',num_topics,{})
+    #Now return the vectorizer, the tfidf and the plot
+    return tfidf_vectorizer, nmf, plot_image
 #A basic function to list the inputs
 def list_options(request):
     options = {
@@ -88,8 +189,6 @@ def train_table(request):
         status_code = status.HTTP_400_BAD_REQUEST
         return response, status_code
     
-    
-
     #Check the Number of Topics
     #If Model Type is NMF, this cannot be blank
     if model_type == 'NMF' and 'num_topics' not in request._full_data:
@@ -122,8 +221,37 @@ def train_table(request):
     else:
         normalisation = "None"
     #If we get this far then things are OK
+    #Now create a model name based on these parameters
+    model_name = filename + '_' + str(selected_column) + '_' + model_type + '_' + str(num_topics) + '_' + normalisation
     #Start by getting the text from the table
     response, status_code = get_table_text(full_file_path,selected_column)
+    
+    #Check the last function was ok
+    if status_code != status.HTTP_200_OK:
+        return response, status_code
+    #If it was ok then it was a dataframe
+    df = response.copy()
+    #Now look to see if we need to do any stemming or Lemmatisation
+    if normalisation == "Stemming":
+        #Call the stemming function on the table
+        df = df.apply(remove_stop_stem)
+        response = df
+    if normalisation == "Lemmatisation":
+        #Call the lemmatisation function on the table
+        df = df.apply(remove_stop_lem)
+        response = df
+    
+    #Now look at the model start with NMF
+    if model_type == 'NMF':
+        #Check the num topics is > 0
+        if num_topics <1:
+            response = {"Message" : "Number of Topics must be greater than 0 for the NMF model"}
+            status_code = status.HTTP_400_BAD_REQUEST
+        #Now train the NMF
+        tfidf_vectorizer, nmf, plot_image = train_nmf(df,num_topics)
+        #Save the plot
+        plot_image.savefig(model_name + '.png')
     return response, status_code
 
+#Next picle model and save to external storage
     
